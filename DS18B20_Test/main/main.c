d22aa0350a585c4bed54aa0a36ba6a4b7b830a59
Manuel Rodriguez <129103279+manu-rodr98@users.noreply.github.com>
2024-06-13 19:33:38 -0300

    Cambiar sensor a DHT11

--- DS18B20_Test/main/main.c
+++ DS18B20_Test/main/main.c
@@ -14,9 +14,8 @@
 #include <esp_log.h>
 #include <esp_err.h>
 
-//Includes - 18B20
-#include <inttypes.h>
-#include <ds18x20.h>
+//Includes - DHT
+#include <dht.h>
 
 //Includes - Wi-Fi
 #include "wifi.h"
@@ -27,11 +26,11 @@
 //Defines
 #define CONFIG_BROKER_URL "mqtt://192.168.43.224:1883"
 #define TOPIC "sensor_1_send"
+#define SENSOR_TYPE DHT_TYPE_DHT11
 
 //Variables
 static const gpio_num_t SENSOR_GPIO = 17;
-static const ds18x20_addr_t SENSOR_ADDR = 0xaf000000162f2828;
-static const char *TAG = "DS18X20";
+static const char *TAG = "DHT11";
 bool status = 0;
 
 //Function to init Wi-Fi station mode
@@ -51,24 +50,22 @@ void init_wifi_STA(void)
 //Main Task
 void test(void *pvParameter)
 {
-    float temperature;
+    float temperature, humidity;
     esp_err_t res;
 
     while (1)
     {
-        res = ds18x20_measure_and_read(SENSOR_GPIO, SENSOR_ADDR, &temperature);
+        res = dht_read_float_data(SENSOR_TYPE, SENSOR_GPIO, &humidity, &temperature);
         if (res != ESP_OK)
-            ESP_LOGE(TAG, "Could not read from sensor %08" PRIx32 "%08" PRIx32 ": %d (%s)",
-                    (uint32_t)(SENSOR_ADDR >> 32), (uint32_t)SENSOR_ADDR, res, esp_err_to_name(res));
+            ESP_LOGE(TAG, "Could not read from sensor: %d (%s)", res, esp_err_to_name(res));
         else
         {
-            ESP_LOGI(TAG, "Sensor %08" PRIx32 "%08" PRIx32 ": %.2f°C",
-                    (uint32_t)(SENSOR_ADDR >> 32), (uint32_t)SENSOR_ADDR, temperature);
+            ESP_LOGI(TAG, "Temperature: %.0f°C, Humidity: %.0f%%", temperature, humidity);
             char temp_str[16];
-            snprintf(temp_str, sizeof(temp_str), "%.2f, 20", temperature);
+            snprintf(temp_str, sizeof(temp_str), "%.0f, %.0f", temperature, humidity);
             mqtt_publish(TOPIC, temp_str);
         }
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        vTaskDelay(pdMS_TO_TICKS(60000));
     }
 }
 
